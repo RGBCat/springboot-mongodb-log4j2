5e9774b56f22c08e5be9b71eeb47df73ddc09c74
hanliqun <760597267@qq.com>
2020-08-21 08:41:44 +0800

    提交

---
+++ demo/src/main/java/com/mongoandlog4j2/demo/DemoApplication.java
@@ -0,0 +1,16 @@
+package com.mongoandlog4j2.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.HashMap;
+
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+}
